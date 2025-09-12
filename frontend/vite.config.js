c30d4cc5df089bf09a568408922b00bc4dd73eb3
Caroline Zhang <caroline.fzhang@gmail.com>
2025-09-12 14:15:55 +1000

    update vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         target: process.env.VITE_API_URL || 'http://localhost:5001',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
